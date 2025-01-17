98c3c904dfb795c7cb6c95750e0e936b288cd851
ibad-ur-rehman-9 <ibadamir021@gmail.com>
2025-01-17 15:54:29 +0500

    Variables and Constants

--- 1-Basics.js
+++ 1-Basics.js
@@ -1,2 +1,10 @@
-console.log("My name is Ibad Ur Rehman")
-console.log("This is the second line")
+// Let , variabales and constants syntax
+
+const accountId = 1.33
+let emailId = "Ibad Ur Rehman"
+var age = 17                        // Out dated keyword hai . Pehle use hota tha , ub sirf let use hota hai lekin kaam dono ka same hai
+
+city = "Jaipur"                     // Ye bhi variable kelie use hota hai but in efficient hai
+let accountNumber                   // Undefined Variable
+
+console.table([accountId, emailId , age, city, accountNumber]);
