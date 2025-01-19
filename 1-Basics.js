a8a6e76e68226a5d6c2c194e88b2f1ef918d6622
ibad-ur-rehman-9 <ibadamir021@gmail.com>
2025-01-19 13:20:21 +0500

    DataTypes conversion

--- 1-Basics.js
+++ 1-Basics.js
@@ -8,3 +8,20 @@ city = "Jaipur"                     // Ye bhi variable kelie use hota hai but in
 let accountNumber                   // Undefined Variable
 
 console.table([accountId, emailId , age, city, accountNumber]);
+
+// Data Types in JavaScript
+
+/*
+1. number = 5 (Range is 2 to the power 52)
+2. bigint = 56666666666666666666666666666666666666666666666666666666666666666666666666666666666(Yani number se bhi ziada range)
+3. string = ""
+4. boolean = true/false
+5. null = ek standalone value hoti hai . It's one of the object in java script
+Can be checked through console.log(typeof null) => Gives object
+6. undefined = undefined is undefined...
+*/
+
+let num = "51"
+console.log(typeof num)
+num = Number(num);
+console.log(typeof num);
