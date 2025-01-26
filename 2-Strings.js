c22f016f25026a307079ad03aec9fc3962f4120f
ibad-ur-rehman-9 <ibadamir021@gmail.com>
2025-01-26 21:38:05 +0500

    Js Basics

---
+++ 2-Strings.js
@@ -0,0 +1,16 @@
+// We can concatenate two strings by using + 
+
+const name = "Ibad Ur Rehman"
+const age = 20
+
+console.log(`My name is ${name} and my age is ${age}` )
+const var1 = String("This is a sample string") // Standard String declaration. By using this , We can use multiple functions associated with strings.
+
+const gameName = String("Minecraft");
+console.log(gameName.length); // For Length
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2)); // Returns the character present at 2nd Position
+console.log(gameName.indexOf('c')); // Returns the index of this particular character
+
+
+
---
+++ 3-NumbersAndMaths.js
@@ -0,0 +1,7 @@
+const var1 = new Number(200.555)
+console.log(var1) // Explicitly tells that this is a number
+
+// Using different built in functions made for number
+console.log(var1.toString());
+console.log(var1.toFixed(1));
+console.log(var1);
